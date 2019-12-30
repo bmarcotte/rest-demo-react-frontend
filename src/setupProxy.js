510dec5f4473b12df8d760c5c61fda05753ac3ae
Ben Marcotte <33464129+bmarcotte@users.noreply.github.com>
2019-12-30 14:57:25 -0800

    Changed how proxy is setup for prod builds

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,14 @@
 const proxy = require( 'http-proxy-middleware' );
+
+const targetURL = process.env.NODE_ENV === 'production'
+  ? 'http://localhost:8080'
+  : 'http://bookmarkapi:8080';
+
 module.exports = function(app) {
   app.use(
     proxy( '/rest', {
-      'target':      'http://localhost:8080',
-      'target-prod': 'http://bookmarkapi:8080',
-      'ws':          true
+      'target': targetURL,
+      'ws': true
     } )
   );
 };
