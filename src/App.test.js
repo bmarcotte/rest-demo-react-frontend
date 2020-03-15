9f9dab57ad9947d57bf94d3f73fe7fc458f8692d
Ben Marcotte <33464129+bmarcotte@users.noreply.github.com>
2020-03-14 23:38:34 -0700

    Added a mock for api calls.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import BookmarkAPI from './components/BookmarkAPI';
+
+describe('App', () => {
+  let performFetchSpy;
+
+  beforeEach(() => {
+    performFetchSpy = jest.spyOn(BookmarkAPI, 'perform_fetch');
+    performFetchSpy.mockResolvedValue(null);
+  });
+
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
