1fd2ad5b00adbca0ef523036f9c2c84f834ba746
Ben Marcotte <33464129+bmarcotte@users.noreply.github.com>
2020-06-06 10:18:38 -0700

    Updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,50 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jest": true,
-        "node": true
+  'env': {
+    'browser': true,
+    'es6': true,
+    'jest': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
     ],
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "no-console": [
-          "error",
-          { "allow": [ "warn", "error" ] }
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    },
-    "settings": {
-      "react": {
-        "pragma": "React",  // Pragma to use, default to "React"
-        "version": "16.5"   // React version, default to the latest React stable release
-      }
+    'no-console': [
+      'error',
+      { 'allow': [ 'warn', 'error' ] }
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  },
+  'settings': {
+    'react': {
+      'pragma': 'React',  // Pragma to use, default to 'React'
+      'version': '16.5'   // React version, default to the latest React stable release
     }
+  }
 };
